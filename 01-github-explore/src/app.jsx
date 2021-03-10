c389b817409987b77b680c01ba35372d220edfc7
Gabriel Mendonça <gabrielmendoncapereira14@gmail.com>
2021-03-10 08:07:35 -0300

    Estilização da listagem

--- 01-github-explore/src/app.jsx
+++ 01-github-explore/src/app.jsx
@@ -1,12 +1,8 @@
-import { Counter } from './components/Counter'
 import { RepositoryList } from './components/RepositoryList'
 import './styles/global.scss'
 
 export function App () { 
   return (
-    <>
-      <RepositoryList />
-      <Counter />
-    </>
+    <RepositoryList />
   )
 }
--- 01-github-explore/src/components/RepositoryList.jsx
+++ 01-github-explore/src/components/RepositoryList.jsx
@@ -1,4 +1,5 @@
 import { RepositoryItem } from "./RepositoryItem";
+import '../styles/repositories.scss'
 
 const repository = {
   name: "Unform",
