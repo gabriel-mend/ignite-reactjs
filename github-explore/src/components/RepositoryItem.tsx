301cdf012378bed95bb4ed694e7612fccd6e5191
Gabriel Mendonça <gabrielmendoncapereira14@gmail.com>
2021-03-16 08:37:21 -0300

    Mudando o nome da pasta

---
+++ github-explore/src/components/RepositoryItem.tsx
@@ -0,0 +1,17 @@
+interface RepositoryItemProps {
+  repository: {
+    name: string
+    description: string
+    html_url: string
+  }
+}
+
+export function RepositoryItem(props: RepositoryItemProps) {
+  return (
+    <li>
+      <strong>{props.repository.name}</strong>
+      <p>{props.repository.description}</p>
+      <a href={props.repository.html_url}>Acessar repositório</a>
+    </li>
+  )
+}
