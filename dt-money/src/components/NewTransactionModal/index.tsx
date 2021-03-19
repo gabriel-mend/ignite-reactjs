1a5964ec0ff2212e19ffa7fc65a20f9ab558e256
Gabriel <gabrielmendoncapereira14@gmail.com>
2021-03-19 09:16:40 -0300

    Estruturando Modal

--- dt-money/src/components/NewTransactionModal/index.tsx
+++ dt-money/src/components/NewTransactionModal/index.tsx
@@ -1,4 +1,5 @@
 import Modal from "react-modal";
+import { Container } from "./styles";
 
 interface NewTransactionModalProps {
   isOpen: boolean
@@ -10,8 +11,28 @@ export function NewTransactionModal ({ isOpen, onRequestClose }: NewTransactionM
     <Modal
       isOpen={isOpen}
       onRequestClose={onRequestClose}
+      overlayClassName="react-modal-overlay"
+      className="react-modal-content"
     >
-      <h2>Cadastrar transação</h2>
+      <Container>
+        <h2>Cadastrar transação</h2>
+        <input 
+          type="text" 
+          placeholder="Título"
+        />
+        <input 
+          type="number" 
+          placeholder="Valor"
+        />
+        <input 
+          type="text" 
+          placeholder="Categoria"
+        />
+
+        <button type="submit">
+          Cadastrar
+        </button>
+      </Container>
     </Modal>
   )
 }
--- dt-money/src/styles/global.ts
+++ dt-money/src/styles/global.ts
@@ -42,5 +42,29 @@ export const GlobalStyles = createGlobalStyle`
       opacity: 0.6;
       cursor: not-allowed;
     }
+
+    .react-modal-overlay {
+      background: rgba(0,0,0,0.5);
+
+      position: fixed;
+      top: 0;
+      left: 0;
+      right: 0;
+      bottom: 0;
+
+      display: flex;
+      align-items: center;
+      justify-content: center;
+    }
+
+    .react-modal-content {
+      width: 100%;
+      max-width: 576px;
+      padding: 3rem;
+      position: relative;
+
+      background-color: ${theme.palette.background};
+      border-radius: 0.25rem;
+    }
   `}
 `
