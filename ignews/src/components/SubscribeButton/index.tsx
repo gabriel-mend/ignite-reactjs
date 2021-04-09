f93dfcdba5a6f53900e0a5765ff0da839a96fa62
Gabriel <gabrielmendoncapereira14@gmail.com>
2021-04-09 09:03:14 -0300

    Redirecionando para a página de checkout

--- ignews/src/components/SubscribeButton/index.tsx
+++ ignews/src/components/SubscribeButton/index.tsx
@@ -1,3 +1,6 @@
+import { signIn, useSession } from 'next-auth/client'
+import { api } from '../../services/api'
+import { getStripeJs } from '../../services/stripe-js'
 import styles from './styles.module.scss'
 
 interface SubscribeButtonProps {
@@ -5,10 +8,30 @@ interface SubscribeButtonProps {
 }
 
 export function SubscribeButton ({ priceID }: SubscribeButtonProps) {
+  const [session] = useSession()
+
+  async function handleSubscribe () {
+    if(!session) {
+      signIn('github')
+      return
+    }
+
+    try {
+      const response = await api.post('/subscribe')
+
+      const { sessionId } = response.data
+
+      const stripe = await getStripeJs()
+      await stripe.redirectToCheckout({ sessionId })
+    } catch(err) {
+      alert(err.message)
+    }
+  }
   return (
     <button
       type="button"
       className={styles.subscribeButton}
+      onClick={handleSubscribe}
     >
       Subscribe Now
     </button>
--- ignews/src/pages/api/auth/[...nextauth].ts
+++ ignews/src/pages/api/auth/[...nextauth].ts
@@ -13,7 +13,7 @@ export default NextAuth({
     }),
   ],
   callbacks: {
-    async signIn(user, account, profile) {
+    async signIn(user) {
       const { email } = user
       
       try {
