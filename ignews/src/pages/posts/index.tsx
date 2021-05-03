2b9a13af4b230812c2105400aaf785fbe537af7a
Gabriel <gabrielmendoncapereira14@gmail.com>
2021-05-03 09:18:17 -0300

    Página: Posts

---
+++ ignews/src/pages/posts/index.tsx
@@ -0,0 +1,32 @@
+import Head from 'next/head'
+import styles from './styles.module.scss'
+
+export default function Posts() {
+  return (
+    <>
+      <Head>
+        <title>Posts | Ignews</title>
+      </Head>
+
+      <main className={styles.container}>
+        <div className={styles.posts}>
+          <a href="">
+            <time>12 de março de 2021</time>
+            <strong>Como renomear vários arquivos de uma vez usando o terminal</strong>
+            <p>Suponha que seu projeto tenha uma base de código com 150 arquivos JavaScript e você precisar migrar para TypeScript alterando as extensões dos arquivos. 🤔</p>
+          </a>
+          <a href="">
+            <time>12 de março de 2021</time>
+            <strong>Como renomear vários arquivos de uma vez usando o terminal</strong>
+            <p>Suponha que seu projeto tenha uma base de código com 150 arquivos JavaScript e você precisar migrar para TypeScript alterando as extensões dos arquivos. 🤔</p>
+          </a>
+          <a href="">
+            <time>12 de março de 2021</time>
+            <strong>Como renomear vários arquivos de uma vez usando o terminal</strong>
+            <p>Suponha que seu projeto tenha uma base de código com 150 arquivos JavaScript e você precisar migrar para TypeScript alterando as extensões dos arquivos. 🤔</p>
+          </a>
+        </div>
+      </main>
+    </>
+  )
+} 
