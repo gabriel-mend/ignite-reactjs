f08fc518fa3060462e60bd545faae27273d02ec1
Gabriel <gabrielmendoncapereira14@gmail.com>
2021-05-03 09:58:44 -0300

    Consumindo API do Prismic

--- ignews/src/pages/posts/index.tsx
+++ ignews/src/pages/posts/index.tsx
@@ -1,4 +1,7 @@
+import { GetStaticProps } from 'next'
+import Prismic from '@prismicio/client'
 import Head from 'next/head'
+import { getPrismicClient } from '../../services/prismic'
 import styles from './styles.module.scss'
 
 export default function Posts() {
@@ -15,18 +18,25 @@ export default function Posts() {
             <strong>Como renomear vários arquivos de uma vez usando o terminal</strong>
             <p>Suponha que seu projeto tenha uma base de código com 150 arquivos JavaScript e você precisar migrar para TypeScript alterando as extensões dos arquivos. 🤔</p>
           </a>
-          <a href="">
-            <time>12 de março de 2021</time>
-            <strong>Como renomear vários arquivos de uma vez usando o terminal</strong>
-            <p>Suponha que seu projeto tenha uma base de código com 150 arquivos JavaScript e você precisar migrar para TypeScript alterando as extensões dos arquivos. 🤔</p>
-          </a>
-          <a href="">
-            <time>12 de março de 2021</time>
-            <strong>Como renomear vários arquivos de uma vez usando o terminal</strong>
-            <p>Suponha que seu projeto tenha uma base de código com 150 arquivos JavaScript e você precisar migrar para TypeScript alterando as extensões dos arquivos. 🤔</p>
-          </a>
         </div>
       </main>
     </>
   )
 } 
+
+export const getStaticProps: GetStaticProps = async () => {
+  const prismic = getPrismicClient()
+
+  const response = await prismic.query([
+    Prismic.predicates.at('document.type', 'post')
+  ], {
+    fetch: ['post.title', 'post.content'],
+    pageSize: 100
+  })
+  console.log(response)
+  return {
+    props: {
+      
+    }
+  }
+}
