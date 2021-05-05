229e4cdd5eba5e4882f08a944e1e4515c418f0af
Gabriel <gabrielmendoncapereira14@gmail.com>
2021-05-05 02:16:48 -0300

    Página: Post

---
+++ ignews/src/pages/posts/[slug].tsx
@@ -0,0 +1,66 @@
+import { GetServerSideProps } from "next"
+import { getSession } from "next-auth/client"
+import { RichText } from "prismic-dom"
+import { getPrismicClient } from "../../services/prismic"
+import Head from 'next/head'
+import styles from './post.module.scss'
+
+interface PostProps {
+  post: {
+    title: string
+    slug: string
+    content: string
+    updatedAt: string
+  }
+}
+
+export default function Post({ post }: PostProps) {
+  return (
+    <>
+      <Head>
+        <title>{post.title} | Ignews</title>
+      </Head>
+
+      <main className={styles.container}>
+        <article className={styles.post}>
+          <h1>{post.title}</h1>
+          <time>{post.updatedAt}</time>
+          <div
+            className={styles.postContent} 
+            dangerouslySetInnerHTML={{ __html: post.content }}
+          />
+        </article>
+      </main>
+    </>
+  )
+}
+
+export const getServerSideProps: GetServerSideProps = async ({ req, params }) => {
+  const session = await getSession({ req })
+  const { slug } = params
+
+  // if(!session) {
+
+  // }
+
+  const prismic = getPrismicClient(req)
+
+  const { data } = await prismic.getByUID('publication', String(slug), {})
+
+  const post = {
+    slug,
+    title: RichText.asText(data.title),
+    content: RichText.asHtml(data.content),
+    updatedAt: new Date(data.last_publication_date).toLocaleDateString('pt-BR', {
+      day: '2-digit',
+      month: 'long',
+      year: 'numeric'
+    })
+  }
+
+  return {
+    props: {
+      post
+    }
+  }
+}
--- ignews/src/pages/posts/index.tsx
+++ ignews/src/pages/posts/index.tsx
@@ -27,7 +27,7 @@ export default function Posts({ posts }: PostsProps) {
       <main className={styles.container}>
         <div className={styles.posts}>
           {posts.map(post => (
-            <Link href="">
+            <Link href={`/posts/${post.slug}`}>
               <a key={post.slug}>
                 <time>{post.updatedAt}</time>
                 <strong>{post.title}</strong>
