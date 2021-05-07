1907f67bfca1149cb15c154b7b41a39b95d9af17
Gabriel <gabrielmendoncapereira14@gmail.com>
2021-05-07 10:31:45 -0300

    Gerando previews estáticos

--- ignews/src/pages/posts/preview/[slug].tsx
+++ ignews/src/pages/posts/preview/[slug].tsx
@@ -1,4 +1,4 @@
-import { GetStaticProps } from "next"
+import { GetStaticPaths, GetStaticProps } from "next"
 import { RichText } from "prismic-dom"
 import { getPrismicClient } from "../../../services/prismic"
 import Head from 'next/head'
@@ -54,7 +54,7 @@ export default function PostPreview({ post }: PostPreviewProps) {
   )
 }
 
-export const getStaticPaths = () => {
+export const getStaticPaths: GetStaticPaths = async () => {
   return {
     paths: [],
     fallback: 'blocking'
