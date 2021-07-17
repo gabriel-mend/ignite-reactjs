ab58fb341c7d71a8fafa02d933d2e28d461b1c4b
Gabriel <gabrielmendoncapereira14@gmail.com>
2021-07-17 07:22:45 -0300

    Componente: Input

--- dashgo/src/pages/index.tsx
+++ dashgo/src/pages/index.tsx
@@ -1,7 +1,44 @@
+import { Flex,  Button, Stack, FormLabel, FormControl } from '@chakra-ui/react' 
+import { Input } from '../components/Form/Input'
 
-
-export default function Home() {
+export default function SignIn() {
   return (
-    <h1>hello world</h1>
+    <Flex
+      w="100vw"
+      h="100vh"
+      align="center"
+      justify="center"
+    >
+      <Flex
+        as="form"
+        width="100%"
+        maxWidth={360}
+        bg="gray.800"
+        p="8"
+        borderRadius={8}
+        flexDirection="column"
+      >
+        <Stack spacing={4}>
+          <Input
+            name="email"
+            type="email"
+            label="E-mail"
+          />
+          <Input
+            name="password"
+            type="password"
+            label="Senha"
+          />
+        </Stack>
+        <Button
+          type="submit"
+          marginTop="6"
+          colorScheme="pink"
+          size="lg"
+        >
+          Entrar
+        </Button>
+      </Flex>
+    </Flex>
   )
 }
