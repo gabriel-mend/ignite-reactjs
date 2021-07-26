a1999787b4a75bf5936d8659b05378a586ebe010
Gabriel <gabrielmendoncapereira14@gmail.com>
2021-07-26 08:56:43 -0300

    Finalizando Header

--- dashgo/src/components/Header.tsx
+++ dashgo/src/components/Header.tsx
@@ -1,5 +1,5 @@
-import { Flex, Input, Text, Icon } from "@chakra-ui/react";
-import { RiSearchLine } from 'react-icons/ri'
+import { Flex, Input, Text, Icon, HStack, Box, Avatar } from "@chakra-ui/react";
+import { RiNotificationLine, RiSearchLine, RiUserAddLine } from 'react-icons/ri'
 
 export function Header () {
   return (
@@ -45,6 +45,34 @@ export function Header () {
           mr="4"
         />
         <Icon as={RiSearchLine}/>
+      </Flex>
+      <Flex
+        align="center"
+        ml="auto"
+      >
+        <HStack
+          spacing="8"
+          marginX="8"
+          pr="8"
+          py="1"
+          color="gray.300"
+          borderRightWidth={1}
+          borderColor="gray.700"
+        >
+          <Icon as={RiNotificationLine} fontSize="20" />
+          <Icon as={RiUserAddLine} fontSize="20" />
+        </HStack>
+        <Flex
+          align="center"
+        >
+          <Box mr="4" textAlign="right">
+            <Text>Gabriel Mendonça</Text>
+            <Text color="gray.300" fontSize="small">crowofcode@gmail.com</Text>
+          </Box>
+          <Avatar size="md" name="Gabriel Mendonça">
+
+          </Avatar>
+        </Flex>
       </Flex>
     </Flex>
   )
--- dashgo/src/pages/dashboard.tsx
+++ dashgo/src/pages/dashboard.tsx
@@ -1,7 +1,20 @@
+import { Flex } from "@chakra-ui/react";
 import { Header } from "../components/Header";
 
 export default function Dashboard () {
   return (
-    <Header />
+    <Flex direction="column" h="100vh">
+      <Header />
+
+      <Flex
+        w="100%"
+        my="6"
+        mx="auto"
+        px="6"
+        maxW={1480}
+      >
+
+      </Flex>
+    </Flex>
   )
 } 
