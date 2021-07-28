e7e5996c89b97a44cd937d5612401600ed7393f0
Gabriel <gabrielmendoncapereira14@gmail.com>
2021-07-28 10:32:43 -0300

    Componente: Pagination

---
+++ dashgo/src/components/Pagination.tsx
@@ -0,0 +1,54 @@
+import { Box, Button, Stack } from "@chakra-ui/react";
+
+export function Pagination () {
+  return (
+    <Stack
+      direction="row"
+      mt="8"
+      justify="space-between"
+      align="center"
+      spacing="6"
+    >
+      <Box>
+        <strong>0</strong> - <strong>10</strong> de <strong>100</strong>
+      </Box>
+      <Stack direction="row" spacing="2">
+        <Button
+          size="sm"
+          fontSize="xs"
+          w="4"
+          colorScheme="pink"
+          disabled
+          _disabled={{
+            bgColor: "pink.500",
+            cursor: "default"
+          }}
+        >
+          1
+        </Button>
+        <Button
+          size="sm"
+          fontSize="xs"
+          w="4"
+          bgColor="gray.700"
+          _hover={{
+            bgColor: 'gray.500'
+          }}
+        >
+          2
+        </Button>
+        <Button
+          size="sm"
+          fontSize="xs"
+          w="4"
+          bgColor="gray.700"
+          _hover={{
+            bgColor: 'gray.500'
+          }}
+        >
+          3
+        </Button>
+      </Stack>
+    </Stack>
+  )
+}
