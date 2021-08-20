0e781bbc846f445217fadab767e7398996f3ce42
Gabriel <gabrielmendoncapereira14@gmail.com>
2021-08-20 17:23:06 -0300

    Responsividade no header

--- dashgo/src/components/Header/Profile.tsx
+++ dashgo/src/components/Header/Profile.tsx
@@ -1,14 +1,20 @@
 import { Avatar, Box, Flex, Text } from "@chakra-ui/react";
 
-export function Profile () {
+interface ProfileProps {
+  showProfileData: boolean
+}
+
+export function Profile ({ showProfileData }: ProfileProps) {
   return (
     <Flex
       align="center"
     >
-      <Box mr="4" textAlign="right">
-        <Text>Gabriel Mendonça</Text>
-        <Text color="gray.300" fontSize="small">crowofcode@gmail.com</Text>
-      </Box>
+      {showProfileData && (
+        <Box mr="4" textAlign="right">
+          <Text>Gabriel Mendonça</Text>
+          <Text color="gray.300" fontSize="small">crowofcode@gmail.com</Text>
+        </Box>
+      )}
       <Avatar size="md" name="Gabriel Mendonça">
 
       </Avatar>
