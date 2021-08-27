78f4b2d3aafd4d6fdfdc85ce05f9a9179d4f33f8
Gabriel <gabrielmendoncapereira14@gmail.com>
2021-08-27 16:42:22 -0300

    Cadastro responsivo

--- dashgo/src/pages/users/create.tsx
+++ dashgo/src/pages/users/create.tsx
@@ -16,17 +16,17 @@ export default function CreateUser () {
       >
         <Sidebar />
 
-        <Box flex="1" borderRadius={8} bgColor="gray.800" p="8">
+        <Box flex="1" borderRadius={8} bgColor="gray.800" p={["6","8"]}>
           <Heading size="lg" fontWeight="normal">Criar usuário</Heading>
 
           <Divider my="6" borderColor="gray.700" />
 
           <VStack spacing="8">
-            <SimpleGrid minChildWidth="240px" spacing="8" w="100%">
+            <SimpleGrid minChildWidth="240px" spacing={["6","8"]} w="100%">
               <Input name="name" label="Nome Completo"/>
               <Input name="email" label="E-mail" type="email" />
             </SimpleGrid>
-            <SimpleGrid minChildWidth="240px" spacing="8" w="100%">
+            <SimpleGrid minChildWidth="240px" spacing={["6","8"]} w="100%">
               <Input name="password" label="Senha" type="password"/>
               <Input name="password_confirmation" label="Confirmar senha" type="password" />
             </SimpleGrid>
