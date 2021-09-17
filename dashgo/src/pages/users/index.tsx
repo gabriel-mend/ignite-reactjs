b80169c796562874fd66306ab0137ed5c3ae9fe1
Gabriel <gabrielmendoncapereira14@gmail.com>
2021-09-17 09:34:17 -0300

    Factories e seeds

--- dashgo/src/pages/users/index.tsx
+++ dashgo/src/pages/users/index.tsx
@@ -1,6 +1,7 @@
 import { Box, Button, Checkbox, Flex, Heading, Icon, Table, Tbody, Td, Text, Th, Thead, Tr, useBreakpointValue } from "@chakra-ui/react";
 import Link from "next/link";
-import { RiAddLine, RiPencilLine } from "react-icons/ri";
+import { RiAddLine } from "react-icons/ri";
+import { useEffect } from "react";
 import { Header } from "../../components/Header";
 import { Pagination } from "../../components/Pagination";
 import { Sidebar } from "../../components/Sidebar";
@@ -11,6 +12,11 @@ export default function UserList () {
     lg: true 
   })
 
+  useEffect(() => {
+    fetch('http://localhost:3000/api/users')
+      .then(response => response.json())
+      .then(data => console.log(data))
+  }, [])
   return (
     <Box>
       <Header />
--- dashgo/src/services/mirage/index.ts
+++ dashgo/src/services/mirage/index.ts
@@ -1,4 +1,5 @@
-import { createServer, Model } from 'miragejs'
+import { createServer, Factory, Model } from 'miragejs'
+import faker from 'faker'
 
 type User = {
   name: string
@@ -11,6 +12,25 @@ export function makeServer() {
     models: {
       user: Model.extend<Partial<User>>({})
     },
+
+    factories: {
+      user: Factory.extend({
+        name(i: number) {
+          return `User ${i + 1}`
+        },
+        email() {
+          return faker.internet.email().toLowerCase()
+        },
+        createdAt() {
+          return faker.date.recent(10)
+        }
+      })
+    },
+
+    seeds(server) {
+      server.createList('user', 200)
+    },
+
     routes() {
       this.namespace = "api";
       this.timing = 750;
