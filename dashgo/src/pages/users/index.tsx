68fc959d151d4a43e99f17a16edac9df0d5d2cde
Gabriel <gabrielmendoncapereira14@gmail.com>
2021-09-30 09:31:22 -0300

    Sinalizando refetch dos dados

--- dashgo/src/pages/users/index.tsx
+++ dashgo/src/pages/users/index.tsx
@@ -8,7 +8,7 @@ import { Sidebar } from "../../components/Sidebar";
 import { useQuery } from 'react-query'
 
 export default function UserList () {
-  const { data, isLoading, error } = useQuery('users', async () => {
+  const { data, isLoading, isFetching, error } = useQuery('users', async () => {
     const response = await fetch('http://localhost:3000/api/users')
     const data = await response.json()
 
@@ -49,7 +49,11 @@ export default function UserList () {
 
         <Box flex="1" borderRadius={8} bgColor="gray.800" p="8">
           <Flex mb="8" justify="space-between" align="center"> 
-            <Heading size="lg" fontWeight="normal">Usuários</Heading>
+            <Heading size="lg" fontWeight="normal">
+              Usuários 
+
+              {!isLoading && isFetching && <Spinner size="sm" color="gray.500" ml="4" />}
+            </Heading>
 
             <Link href="/users/create" passHref>
               <Button
