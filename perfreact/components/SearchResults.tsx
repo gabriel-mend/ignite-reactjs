5a7f915d514c4be29ec17514562bacca74c836dc
Gabriel <gabriel.pereira@ceuma.br>
2022-01-09 03:22:12 -0300

    useMemo

--- perfreact/components/SearchResults.tsx
+++ perfreact/components/SearchResults.tsx
@@ -1,21 +1,27 @@
-import { ProductItem } from "./ProductItem"
+import { useMemo } from "react";
+import { ProductItem } from "./ProductItem";
 
 interface SearchResultsProps {
   results: Array<{
-    id: number
-    price: number
-    title: string
-  }>
+    id: number;
+    price: number;
+    title: string;
+  }>;
 }
 
-export function SearchResults ({ results }: SearchResultsProps) {
+export function SearchResults({ results }: SearchResultsProps) {
+  const totalPrice = useMemo(() => {
+    results.reduce((total, product) => {
+      return total + product.price;
+    }, 0);
+  }, [results]);
+
   return (
     <div>
-      {results.map(product => {
-        return (
-          <ProductItem product={product} key={product.id}/>
-        )
+      <h1>Total: {totalPrice}</h1>
+      {results.map((product) => {
+        return <ProductItem product={product} key={product.id} />;
       })}
     </div>
-  )
+  );
 }
