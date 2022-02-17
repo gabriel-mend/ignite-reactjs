f254f15b79fc53a339ad7e013cebabfcfccad7f8
gabriel <gabriel.pereira@ceuma.br>
2022-02-17 15:13:36 -0300

    Testando primeiro componente

--- ignews/jest.config.js
+++ ignews/jest.config.js
@@ -1,9 +1,9 @@
 module.exports = {
-  testIgnorePatterns: ["/node_modules/", "/.next/"],
+  testPathIgnorePatterns: ["/node_modules/", "/.next/"],
   setupFilesAfterEnv: [
     "<rootDir>/src/tests/setupTests.ts"
   ],
-  trasnform: {
+  transform: {
     "^.+\\.(js|jsx|ts|tsx)": "<rootDir>/node_modules/babel-jest"
   },
   testEnvironment: 'jsdom'
