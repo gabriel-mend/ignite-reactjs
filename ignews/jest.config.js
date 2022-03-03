0cd09bca4edccc7eac66f8f816fd072280e630fc
Gabriel <gabriel.pereira@ceuma.br>
2022-03-03 09:17:12 -0300

    Testando Header

--- ignews/jest.config.js
+++ ignews/jest.config.js
@@ -6,5 +6,8 @@ module.exports = {
   transform: {
     "^.+\\.(js|jsx|ts|tsx)": "<rootDir>/node_modules/babel-jest"
   },
-  testEnvironment: 'jsdom'
+  testEnvironment: 'jsdom',
+  moduleNameMapper: {
+    "\\.(scss|css|sass)": "identity-obj-proxy"
+  } 
 }
